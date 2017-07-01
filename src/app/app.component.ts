351f4b0cf6de33b94ce37a70f0e982eb0927863d
flavglen <flavglen@gmail.com>
2017-07-01 20:52:31 +0530

    awfawfawfwafwafwafwafwaf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent   implements OnInit {
   title = 'app';
+  files=[];
+ngOnInit() {
+
+    this.files=  [
+          {
+              "label": "Documents",
+              "id":1,
+              "data": "Documents Folder",
+              "expandedIcon": "fa-folder-open",
+              "collapsedIcon": "fa-folder",
+              "isVisible":true,
+              "children": [{
+                      "label": "Work",
+                      "id":2,
+                      "data": "Work Folder",
+                      "expandedIcon": "fa-folder-open",
+                      "collapsedIcon": "fa-folder",
+                      "isVisible":true,
+                  },
+                  {
+                      "label": "Home",
+                      "id":3,
+                      "data": "Home Folder",
+                      "expandedIcon": "fa-folder-open",
+                      "collapsedIcon": "fa-folder",
+                      "isVisible":true,
+                  }]
+          },
+
+          {
+              "label": "Movies12",
+              "id":4,
+              "data": "Movies Folder",
+              "expandedIcon": "fa-folder-open",
+              "collapsedIcon": "fa-folder",
+              "isVisible":true,
+              "children": [{
+                      "label": "Work2",
+                      "id":5,
+                      "data": "Work Folder",
+                      "expandedIcon": "fa-folder-open",
+                      "collapsedIcon": "fa-folder",
+                      "isVisible":true,
+                  },
+                  {
+                      "label": "Home2",
+                      "id":6,
+                      "data": "Home Folder2",
+                      "expandedIcon": "fa-folder-open",
+                      "collapsedIcon": "fa-folder",
+                      "isVisible":true,
+                  }]
+          }
+      ]
+}
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MytreeComponent,UITreeNode,TreeNodeTemplateLoader } from './mytree/mytree.component';
+import { MytreemoduleModule } from './mytreemodule/mytreemodule.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MytreemoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
